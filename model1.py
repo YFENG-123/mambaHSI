1c8d34c9033b459681f405aae4d46e02c7c53932
YFENG-123 <578851062@qq.com>
2025-12-08 20:55:15 +0800

    Update convolutional layer in Net1 to use a 3x3 kernel for improved feature extraction and spatial representation.

--- model1.py
+++ model1.py
@@ -44,7 +44,7 @@ class Net1(nn.Module):
             nn.BatchNorm2d(256),
             nn.GELU(),
             nn.Dropout(dropout_rate),
-            nn.Conv2d(256, 128, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.GELU(),
             nn.Dropout(dropout_rate),
