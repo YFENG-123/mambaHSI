052164579ba60572ffdb737989932518d5da9dca
YFENG-123 <578851062@qq.com>
2025-12-16 10:27:09 +0800

    Update .gitignore and main.py: Add 'data/' to ignored files, adjust learning rate and dropout rate for training, and restore previously commented image and ground truth paths for consistency in dataset usage.

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ program_start_time = time.time()
 
 ################################# 设置超参数 #################################
 num_epochs = 1000  # 训练轮数
-learning_rate = 5e-3  # 适配 Pre-Norm Residual 结构
-dropout_rate = 0.4
+learning_rate = 1e-3  # 适配 Pre-Norm Residual 结构
+dropout_rate = 0.5
 ################################# 优化器参数 ##################################
 optimizer_type = "Adam"  # 优化器类型: "SGD", "Adam", "AdamW", "RMSprop", "Adagrad"
 # SGD 参数（随机梯度下降）
@@ -75,26 +75,26 @@ seeds = [
 ]
 image_paths = [
     "data/HuaiLai.mat",
-    #"data/Botswana.mat",
-    #"data/Indian_pines.mat",
-    #"data/KSC.mat",
-    #"data/Pavia.mat",
-    #"data/PaviaU.mat",
-    #"data/Salinas.mat",
+    "data/Botswana.mat",
+    "data/Indian_pines.mat",
+    "data/KSC.mat",
+    "data/Pavia.mat",
+    "data/PaviaU.mat",
+    "data/Salinas.mat",
     #"data/SalinasA.mat",
 ]
 gt_paths = [
     "data/HuaiLai_gt.mat",
-    #"data/Botswana_gt.mat",
-    #"data/Indian_pines_gt.mat",
-    #"data/KSC_gt.mat",
-    #"data/Pavia_gt.mat",
-    #"data/PaviaU_gt.mat",
-    #"data/Salinas_gt.mat",
+    "data/Botswana_gt.mat",
+    "data/Indian_pines_gt.mat",
+    "data/KSC_gt.mat",
+    "data/Pavia_gt.mat",
+    "data/PaviaU_gt.mat",
+    "data/Salinas_gt.mat",
     #"data/SalinasA_gt.mat",
 ]
-val_split_rate = 0.45
-test_split_rate = 0.45
+val_split_rate = 0.1
+test_split_rate = 0.8
 
 ################################# 打印超参数 ##################################
 print(f"训练轮数:{num_epochs}\t\t学习率:{learning_rate}\t\tDropout率:{dropout_rate}")
